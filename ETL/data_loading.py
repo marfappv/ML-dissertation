aa223a6a453aa3d6f1c1bb2dcd4f1e31d736161c
Your Name <you@example.com>
2022-06-06 09:33:58 +0100

    Edits data loading script based on postgres connection

--- ETL/data_loading.py
+++ ETL/data_loading.py
@@ -1,5 +1,3 @@
-!pip install findspark
-
 import os
 os.environ["JAVA_HOME"] = "/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home"
 os.environ["SPARK_HOME"] = "/Users/Marfa-Popova/data_eng_ind/spark-3.2.1-bin-hadoop3.2"
@@ -12,91 +10,20 @@ from pyspark.sql import SQLContext
 sc = pyspark.SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
 
-assets_showroom = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/showroom.parquet", header=True)
-assets_API = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/assets_API.parquet", header=True)
-assets_ws = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/opensea_ws.parquet", header=True)
-collections = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/collections_API.parquet", header=True)
-finances = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/finances_API.parquet", header=True)
-socials = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/socials_API.parquet", header=True)
-urls = sqlContext.read.parquet("s3a://data-eng-ind/parquet-files/urls_API.parquet", header=True)
+projects = sqlContext.read.parquet('\Users\MarfaPopova\S2R Analytics\Development & Support Team - Power BI for Synergy - Advanced Analytics\DataFlowExtract\ETL\parquet-files\projects.parquet', header=True)
+clients = sqlContext.read.parquet('\Users\MarfaPopova\S2R Analytics\Development & Support Team - Power BI for Synergy - Advanced Analytics\DataFlowExtract\ETL\parquet-files\clients.parquet', header=True)
+stages = sqlContext.read.parquet('\Users\MarfaPopova\S2R Analytics\Development & Support Team - Power BI for Synergy - Advanced Analytics\DataFlowExtract\ETL\parquet-files\stages.parquet', header=True)
+transactions = sqlContext.read.parquet('\Users\MarfaPopova\S2R Analytics\Development & Support Team - Power BI for Synergy - Advanced Analytics\DataFlowExtract\ETL\parquet-files\transactions.parquet', header=True)
+staff = sqlContext.read.parquet('\Users\MarfaPopova\S2R Analytics\Development & Support Team - Power BI for Synergy - Advanced Analytics\DataFlowExtract\ETL\parquet-files\staff.parquet', header=True)
 
-postgres_uri = "jdbc:postgresql://opensea.c5pkb2dzarva.us-west-2.rds.amazonaws.com:5432/opensea"
-user = "marfapopova21"
-password = "qwerty123"
-
-assets_showroom.write \
-    .mode("append") \
-    .format("jdbc") \
-    .option("dbtable", "nfts.assets") \
-    .option("url", postgres_uri) \
-    .option("user", user) \
-    .option("password", password) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
-
-assets_API.write \
-    .mode("append") \
-    .format("jdbc") \
-    .option("url", postgres_uri) \
-    .option("user", user) \
-    .option("dbtable", "nfts.assets") \
-    .option("password", password) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
-
-assets_ws.write \
-    .mode("append") \
-    .format("jdbc") \
-    .option("dbtable", "nfts.assets") \
-    .option("url", postgres_uri) \
-    .option("password", password) \
-    .option("user", user) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
-
-collections.write \
-    .format("jdbc") \
-    .mode("append") \
-    .option("url", postgres_uri) \
-    .option("dbtable", "nfts.collections") \
-    .option("user", user) \
-    .option("password", password) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
-
-finances.write \
-    .format("jdbc") \
-    .option("dbtable", "nfts.finances") \
-    .mode("append") \
-    .option("password", password) \
-    .option("url", postgres_uri) \
-    .option("user", user) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
-
-socials.write \
-    .format("jdbc") \
-    .mode("append") \
-    .option("url", postgres_uri) \
-    .option("user", user) \
-    .option("dbtable", "nfts.socials") \
-    .option("driver", "org.postgresql.Driver") \
-    .option("password", password) \
-    .save()
-
-urls.write \
-    .format("jdbc") \
-    .mode("append") \
-    .option("dbtable", "nfts.urls") \
-    .option("url", postgres_uri) \
-    .option("user", user) \
-    .option("password", password) \
-    .option("driver", "org.postgresql.Driver") \
-    .save()
+#postgres_uri = "jdbc:postgresql://opensea.c5pkb2dzarva.us-west-2.rds.amazonaws.com:5432/opensea"
+#user = "marfapopova21"
+#password = "qwerty123"
 
 
 # Create a database and connect to it
-conn = sqlite3.connect('WGA.db')
+import sqlite3
+conn = sqlite3.connect('wga.db')
 
 # Connecting to a database created in MS SQL Server Management Studio
 import pyodbc
@@ -112,3 +39,54 @@ cursor = cnxn.cursor()
 sql_statement = "select 1"
 response = cursor.execute(sql_statement).fetchone()
 print(response[0])
+
+# Write data tables ino the database
+staff.write \
+    .format("jdbc") \
+    .option("dbtable", "wga.staff") \
+    .mode("append") \
+    .option("password", password) \
+    .option("url", cnxn) \
+    .option("user", username) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+
+projects.write \
+    .mode("append") \
+    .format("jdbc") \
+    .option("dbtable", "wga.projects") \
+    .option("url", cnxn) \
+    .option("user", username) \
+    .option("password", password) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+    
+stages.write \
+    .mode("append") \
+    .format("jdbc") \
+    .option("dbtable", "wga.stages") \
+    .option("url", cnxn) \
+    .option("password", password) \
+    .option("user", username) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+
+clients.write \
+    .mode("append") \
+    .format("jdbc") \
+    .option("url", cnxn) \
+    .option("user", username) \
+    .option("dbtable", "wga.clinets") \
+    .option("password", password) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
+
+transactions.write \
+    .format("jdbc") \
+    .mode("append") \
+    .option("url", cnxn) \
+    .option("dbtable", "wga.transactions") \
+    .option("user", username) \
+    .option("password", password) \
+    .option("driver", "org.postgresql.Driver") \
+    .save()
